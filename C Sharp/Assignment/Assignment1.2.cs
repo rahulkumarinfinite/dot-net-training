5f91c4ebce545d2ff0dc60f38d71ab0c6a0224a1
RahulKumar <rahulkumar@infinite.com>
2025-06-18 17:04:00 +0530

    Assignment1Q2

---
+++ C Sharp/Assignment/Assignment1.2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Assignment1Q2
+{
+    class Program
+    {
+        //To check the number is positive o
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a > 0)
+            {
+                Console.WriteLine("{0} is positive number", a);
+            }
+            else if (a < 0)
+                Console.WriteLine("{0} is negative number", a);
+            else
+                Console.WriteLine("Zero is neutral number");
+            Console.Read();
+        }
+    }
+}
