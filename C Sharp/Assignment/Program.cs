b7950beb684df2d53251caa4dcaadd3219416df4
RahulKumar <rahulkumar@infinite.com>
2025-06-18 14:36:32 +0530

    Addassignment1Q1

---
+++ C Sharp/Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the two number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a == b)
+            {
+                Console.WriteLine("Given number are equal");
+            }
+            else
+            {
+                Console.WriteLine("Given number are not equal");
+            }
+            Console.Read();
+        }
+    }
+}
