d7aae358bc8e482a2f7d8cb23c28ae23f644948e
RahulKumar <rahulkumar@infinite.com>
2025-06-20 17:07:07 +0530

    Assignment2

---
+++ C Sharp/Assignment/Assignment2.3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+/*
+  Write a C# Sharp program to read any day number as an integer and display the name of the day as a word.
+
+Test Data / input: 2
+
+Expected Output :
+Tuesday
+*/
+
+namespace Assignment2._3
+{
+    public enum day { sunday,monday,tuesday,wednesday,thursday,friday,saturday}; //initialise the day enumerations
+
+    class Program
+    {
+        static void ReadDay(int a) //function
+        {
+            Console.WriteLine("the day is :"+Enum.GetName(typeof(day), a));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int a = Convert.ToInt32(Console.ReadLine());  //taking the day
+            ReadDay(a); //calling the function
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ C Sharp/Assignment/Assignment2String2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+//Write a program in C# to accept a word from the user and display the reverse of it. 
+
+namespace Assignment2String2
+{
+    class Solution
+    {
+        public void Reverse(string s)
+        {
+           string str=new string(s.Reverse().ToArray());
+
+            Console.WriteLine("The reverse of word will be "+str);
+
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine("Input the word");
+            string s = Console.ReadLine();
+            solution.Reverse(s);
+            Console.Read();
+        }
+    }
+}
